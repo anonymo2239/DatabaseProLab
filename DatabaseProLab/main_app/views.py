6f3684c2a54a7590dd09c6ba8f56e67958bce8ce
Adem Alperen Arda <alperen.arda.adem22@gmail.com>
2024-05-13 23:43:15 +0300

    database binded

--- DatabaseProLab/main_app/views.py
+++ DatabaseProLab/main_app/views.py
@@ -7,20 +7,26 @@ from .models import Doktorlar
 from .models import Hastalar
 from .models import Yonetici
 
+
 def home(request):
     return render(request, "main_app/mainpage.html")
+
 
 def admin(request):
     return render(request, "main_app/admin/admin.html")
 
+
 def admin_first(request):
     return render(request, "main_app/admin/adminfirst.html")
+
 
 def doctors(request):
     return render(request, "main_app/doctors/doctors.html")
 
+
 def patients(request):
     return render(request, "main_app/patients/patients.html")
+
 
 # SAYFALARI SQL İLE BAĞLAMA
 
@@ -38,14 +44,28 @@ def patient_login(request):
         else:
             return HttpResponseRedirect('/hasta/')  # Kök URL'ye yönlendir
 
+
 def admin_second_patient(request):
-    return render(request, 'main_app/admin/adminsecond_patient.html')
+    if request.method == 'GET':
+        hastalar = Hastalar.objects.all()
+        return render(request, 'main_app/admin/adminsecond_patient.html', {'hastalar': hastalar})
+    elif request.method == 'POST':
+        # POST request ile gönderilen verileri işleme kodu buraya eklenebilir
+        pass
+
 
 def admin_second_doctor(request):
-    return render(request, 'main_app/admin/adminsecond_doctor.html')
+    if request.method == 'GET':
+        doktorlar = Doktorlar.objects.all()
+        return render(request, 'main_app/admin/adminsecond_doctor.html', {'doktorlar': doktorlar})
+    elif request.method == 'POST':
+        # POST request ile gönderilen verileri işleme kodu buraya eklenebilir
+        pass
+
 
 def admin_second_report(request):
     return render(request, 'main_app/admin/adminsecond_report.html')
+
 
 def yonetici_login(request):
     if request.method == 'POST':
@@ -57,7 +77,13 @@ def yonetici_login(request):
             user = cursor.fetchone()
 
         if user is not None:
-            return render(request, "main_app/patients/patients_main_page.html")
+            return render(request, "main_app/admin/adminfirst.html")
         else:
             return HttpResponseRedirect('/yonetici/giris/')
+
+    return render(request, 'main_app/admin/adminfirst.html')
+
+
+# admin hasta düzenleme bölümüne sql tablosunu aktarmak
+
 
